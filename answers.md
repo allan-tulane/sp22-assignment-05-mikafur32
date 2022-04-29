be0b447d8ed0f203accf11b81ed6345ebb6b57f9
mikafur32 <msison1@tulane.edu>
2022-04-28 22:20:35 -0500

    assignment 5

--- answers.md
+++ answers.md
@@ -1,24 +1,59 @@
 # CMPS 2200 Assignment 5
+
 ## Answers
 
-**Name:**_________________________
-
+**Name:** Mikey Sison
 
 Place all written answers from `assignment-05.md` here for easier grading.
 
-
-
-
-
 - **1a.**
 
+The greedy algorithm would select the largest coin size that would fit in given dollar amount. The algorithm would then
+select the next largest, then the next largest, so on and so forth until reaching the dollar amount.
+
+- **1b.**
+  Work = O(log n)
+  Span = O(log n)
 
 
-
-
+- **2a.**
+  Denominations: $5, $4, $1.
+- To make $8, greedy method would take 5 + 1 + 1 + 1 = 8, using 4 bills, when in reality, you could just use 2 $4 bills.
+- A Greedy algorithm would fail to account for scenarios where the second or third largest * x would yield the lowest
+  number of coins.
 
 
 - **2b.**
 
+---
 
+if Dk can be used
+((n - Dk, k) + 1)
 
+if Dk cant be used
+(n, k - 1)
+
+min(f(n - Dk, k) + 1, f(n, k[1:])),
+where n = amount of money and k is the number of given denominations.
+
+- Given the set of denominations, S = {5, 4, 1}, a memoized table would look like this:
+  Memoized table
+-
+
+|$    |  5    | 4    |  1 | min value to lookup|
+|-------|-------|------|------|----|
+|0      |   0  | 0     | 0     |  0  |
+|1      |   0  | 0     | 1     |  1  |
+|2      |   0  | 0     | 2     |  2  |
+|3      |   0  | 0     | 3     |  3  |
+|4      |   0  | 1     | 4     |  1  |
+|5      |   1  | 2     | 5     |  1  |
+|6      |   2  | 3     | 6     |  2  |
+|7      |   3  | 4     | 7     |  3  |
+|8      |   4  | 2     | 8     |  2  |
+|9      |   2  | 3     | 9     |  2  |
+|10     |   2  | 4     | 10    |  2  |
+|11     |   3  | 5     | 11    |  3  |
+
+So, with a memoized version of the DAG, 
+similar to the knapsack problem, would have a work of O(n*k) and span of O(n).
--- main.py
+++ main.py
@@ -1,36 +1,165 @@
-
 ####### Problem 3 #######
 
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
+test_cases = [('book', 'back'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA'), ('kookaburra', 'kookybird')]
+alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant', '-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
+
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
     if (S == ""):
-        return(len(T))
+        return (len(T))
     elif (T == ""):
-        return(len(S))
+        return (len(S))
     else:
         if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+            return (MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return (1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:])))
 
 
-def fast_MED(S, T, MED={}):
-    # TODO -  implement memoization
-    pass
+def fast_MED(S, T):
+    def pad(x, y):
+        # pad with leading 0 if x/y have different number of bits
+        # e.g., [1,0] vs [1]
+        pad_length = 0
+        pad_bool = True
+        if len(x) < len(y):
+            pad_length = (len(y) - len(x))
+            pad_bool = True
+            x = x + '0' * pad_length
+        elif len(y) < len(x):
+            pad_length = (len(x) - len(y))
+            pad_bool = False
+            y = y + '0' * pad_length
+        return x, y, pad_length, pad_bool
 
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    S, T, pad_length, pad_bool = pad(S, T)
+
+    memo_arr = [[0 for n in range(len(S))] for m in range(len(T))]
+
+    for i in range(len(S)):
+        for j in range(len(T)):
+            if i == 0:
+                memo_arr[i][j] = j
+
+            elif j == 0:
+                memo_arr[i][j] = i
+            else:
+                if S[i] == T[j]:
+                    memo_arr[i][j] = memo_arr[i - 1][j - 1]
+                else:
+                    memo_arr[i][j] = 1 + min(memo_arr[i - 1][j - 1], memo_arr[i][j - 1], memo_arr[i - 1][j])
+    # format_arr(memo_arr,S,T)
+
+    if pad_bool:
+        # print(((len(S) - pad_length - 1), len(T) - 1))
+        return memo_arr[len(S) - pad_length - 1][len(T) - 1]  # TODO verify
+
+    else:
+        # print(((len(S) - 1), len(T) - pad_length - 1))
+        return memo_arr[len(S) - 1][len(T) - pad_length - 1]  # TODO verify
+
+
+def format_arr(arr, S, T):
+    print(list(T))
+    for i in range(len(arr)):
+        print((S[i]), arr[i])
+
+
+def fast_MED_arr(S, T):
+    def pad(x, y):
+        # pad with leading 0 if x/y have different number of bits
+        # e.g., [1,0] vs [1]
+        pad_length = 0
+        pad_bool = True
+        if len(x) < len(y):
+            pad_length = (len(y) - len(x))
+            pad_bool = True
+            x = x + '0' * pad_length
+        elif len(y) < len(x):
+            pad_length = (len(x) - len(y))
+            pad_bool = False
+            y = y + '0' * pad_length
+        return x, y, pad_length, pad_bool
+
+    S, T, pad_length, pad_bool = pad(S, T)
+
+    memo_arr = [[0 for n in range(len(S))] for m in range(len(T))]
+
+    for i in range(len(S)):
+        for j in range(len(T)):
+            if i == 0:
+                memo_arr[i][j] = j
+
+            elif j == 0:
+                memo_arr[i][j] = i
+            else:
+                if S[i] == T[j]:
+                    memo_arr[i][j] = memo_arr[i - 1][j - 1]
+                else:
+                    memo_arr[i][j] = 1 + min(memo_arr[i - 1][j - 1], memo_arr[i][j - 1], memo_arr[i - 1][j])
+    # format_arr(memo_arr,S,T)
+    return memo_arr
+
+
+def fast_align_MED(S, T):
+    # fast med traveerse back from the memoTable
+    # if up, append S original, append - to T
+    # if down, append T original, append - to S
+    # always choose up if both are equal
+    arr = fast_MED_arr(S, T)
+    alignS, alignT = '',''
+
+    i = len(S) - 1
+    j = len(T) - 1
+    while i >= 0:
+        while j >= 0:
+            x = arr[i][j - 1]  #up
+            y = arr[i - 1][j - 1] #diag
+            z = arr[i - 1][j] # left
+            if z <= x and z <= y:
+                alignS = S[i] + alignS
+                alignT = "-" + alignT
+                #go left
+                i -= 1
+
+            elif y <= x and y <= z:
+                alignS = S[i] + alignS
+                alignT = T[j] + alignT
+
+                # go diagonal
+                i -= 1
+                j -= 1
+
+            elif x <= y and x <= z:
+                # go up
+                alignS = '-' + alignS
+                alignT = T[j] + alignT
+                j -= 1
+
+
+
+
+
+
+
+    return alignS, alignT
+
 
 def test_MED():
     for S, T in test_cases:
+        print(fast_MED(S, T))
+        print(MED(S, T))
         assert fast_MED(S, T) == MED(S, T)
-                                 
+
+
 def test_align():
     for i in range(len(test_cases)):
         S, T = test_cases[i]
         align_S, align_T = fast_align_MED(S, T)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+
+
+for i in test_cases:
+    print(fast_align_MED(i[0], i[1]))
+
+#print(fast_align_MED('kookaburra', 'kookybird'))
